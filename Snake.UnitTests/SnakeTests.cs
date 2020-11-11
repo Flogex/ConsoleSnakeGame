45be2aa8b89df0961b80125ab0d00fabefe90fde
Florian Gerlinghoff <gerlinghoff@live.de>
2020-11-11 20:15:49 +0100

    Move snake with tail

--- Snake.UnitTests/SnakeTests.cs
+++ Snake.UnitTests/SnakeTests.cs
@@ -1,11 +1,12 @@
-﻿using FluentAssertions;
+﻿using System.Linq;
+using FluentAssertions;
 using Xunit;
 
 namespace Snake.UnitTests
 {
     public class SnakeTests
     {
-        private static readonly Position _anyPosition = new Position(1, 1);
+        private static readonly Position _anyPosition = new Position(2, 2);
 
         public class WhenCreatingNewSnake
         {
@@ -13,11 +14,16 @@ namespace Snake.UnitTests
             [InlineData(1, 1)]
             public void ThenHeadShouldBeAtGivenPosition(int x, int y)
             {
-                var headPosition = new Position(x, y);
-                var snake = new Snake(headPosition);
-
+                var snake = new Snake(x, y);
                 snake.Head.X.Should().Be(x);
                 snake.Head.Y.Should().Be(y);
+            }
+
+            [Fact]
+            public void ThenBodyShouldContainHeadAsSingleElement()
+            {
+                var snake = new Snake(2, 2);
+                snake.Body.Should().ContainSingle(p => p == new Position(2, 2));
             }
 
             [Fact]
@@ -69,13 +75,40 @@ namespace Snake.UnitTests
             }
         }
 
+        public class WhenMovingSnakeWithTail
+        {
+            private readonly Snake _snake;
+
+            public WhenMovingSnakeWithTail()
+            {
+                _snake = new Snake(3, 3);
+                _snake.Move(Direction.Left);
+                _snake.Eat();
+            }
+
+            [Fact]
+            // \forall s \in Snakes:
+            //    s' = s.Move(Direction.Left)
+            //    s'.Body[1..] == s.Body[0..^2]
+            public void ThenAllElementsShouldMoveToPositionOfPredecessor()
+            {
+                _snake.Move(Direction.Left);
+                _snake.Body.Should().ContainInOrder(new Position(1, 3), new Position(2, 3));
+            }
+        }
+
         public class WhenEating
         {
+            private static readonly Direction _anyDirection = Direction.Right;
+
             [Fact]
             public void AndSnakeHasNoTail_LengthOfSnakeShouldBe2()
             {
                 var snake = new Snake(_anyPosition);
+                snake.Move(_anyDirection);
+
                 snake.Eat();
+
                 snake.Length.Should().Be(2);
             }
 
@@ -83,9 +116,23 @@ namespace Snake.UnitTests
             public void AndSnakeHasLengthOf2_LengthOfSnakeShouldBe3()
             {
                 var snake = new Snake(_anyPosition);
+                snake.Move(_anyDirection);
                 snake.Eat();
+
                 snake.Eat();
+
                 snake.Length.Should().Be(3);
+            }
+
+            [Theory]
+            [InlineData(2, 2)]
+            [InlineData(2, 3)]
+            public void NewTailElementShouldAppearOnPreviousPositionOfLastTailPart(int x, int y)
+            {
+                var snake = new Snake(x, y);
+                snake.Move(Direction.Left);
+                snake.Eat();
+                snake.Body.Last().Should().Be(new Position(x, y));
             }
         }
     }
--- Snake/Snake.cs
+++ Snake/Snake.cs
@@ -1,44 +1,49 @@
-﻿using static Snake.Direction;
+﻿using System;
+using System.Collections.Generic;
+using static Snake.Direction;
 
 namespace Snake
 {
     public class Snake
     {
+        private Position? _previousLastPartPosition;
+
         public Snake(int x, int y) : this(new Position(x, y)) { }
 
-        public Snake(Position initialHeadPosition)
+        public Snake(Position initialHead)
         {
-            this.Head = initialHeadPosition;
+            this.Body = new List<Position> { initialHead };
         }
 
-        public Position Head { get; private set; }
+        public Position Head => this.Body[0];
 
-        public int Length { get; private set; } = 1;
+        public List<Position> Body { get; }
+
+        public int Length => this.Body.Count;
 
         public void Move(Direction direction)
         {
-            switch(direction)
+            _previousLastPartPosition = this.Body[^1];
+
+            var newHeadPosition = direction switch
             {
-                case Left: MoveLeft(); break;
-                case Right: MoveRight(); break;
-                case Up: MoveUp(); break;
-                case Down: MoveDown(); break;
+                Left => this.Head.WithX(this.Head.X - 1),
+                Right => this.Head.WithX(this.Head.X + 1),
+                Up => this.Head.WithY(this.Head.Y - 1),
+                Down => this.Head.WithY(this.Head.Y + 1),
+                _ => throw new NotSupportedException("Unknown direction")
             };
+
+            this.Body.RemoveAt(this.Body.Count - 1);
+            this.Body.Insert(0, newHeadPosition);
         }
 
-        private void MoveLeft() =>
-            this.Head = this.Head.WithX(this.Head.X - 1);
+        public void Eat()
+        {
+            if (!_previousLastPartPosition.HasValue)
+                throw new InvalidOperationException("You have to move before you can eat");
 
-        private void MoveRight() =>
-            this.Head = this.Head.WithX(this.Head.X + 1);
-
-        private void MoveUp() =>
-            this.Head = this.Head.WithY(this.Head.Y - 1);
-
-        private void MoveDown() =>
-            this.Head = this.Head.WithY(this.Head.Y + 1);
-
-        public void Eat() =>
-            this.Length++;
+            this.Body.Add(_previousLastPartPosition.Value);
+        }
     }
 }
