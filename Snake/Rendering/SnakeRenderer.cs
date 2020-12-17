8e3d0d2bd6554e810eedceceab8cc170e84de7fb
Florian Gerlinghoff <gerlinghoff@live.de>
2020-12-17 14:43:47 +0100

    Optimized implementation of SnakeRenderer

--- Snake/Rendering/SnakeRenderer.cs
+++ Snake/Rendering/SnakeRenderer.cs
@@ -3,29 +3,38 @@
     internal class SnakeRenderer
     {
         private readonly IConsole _console;
+        private Snake? _currentSnake;
 
         public SnakeRenderer(IConsole console)
         {
             _console = console;
         }
 
-        public void RenderNext(Snake snake)
+        public void RenderNext(Snake nextSnake)
         {
-            ClearConsole();
+            // Assumption: The next snake rendered has only moved one field.
 
-            foreach (var part in snake.Body)
-                _console.WriteAt('X', part.Y, part.X);
+            if (_currentSnake.HasValue)
+                RenderSnakeDiff(_currentSnake.Value, nextSnake);
+            else
+                RenderFullSnake(nextSnake);
+
+            _currentSnake = nextSnake;
         }
 
-        private void ClearConsole()
+        private void RenderSnakeDiff(Snake currentSnake, Snake nextSnake)
         {
-            for (var row = 0; row < _console.Height; row++)
-            {
-                for (var column = 0; column < _console.Width; column++)
-                {
-                    _console.RemoveCharAt(row, column);
-                }
-            }
+            var partToRemove = currentSnake.Body[^1];
+            _console.RemoveCharAt(partToRemove.Y, partToRemove.X);
+
+            var partToAdd = nextSnake.Head;
+            _console.WriteAt('X', partToAdd.Y, partToAdd.X);
+        }
+
+        private void RenderFullSnake(Snake snake)
+        {
+            foreach (var part in snake.Body)
+                _console.WriteAt('X', part.Y, part.X);
         }
     }
 }
