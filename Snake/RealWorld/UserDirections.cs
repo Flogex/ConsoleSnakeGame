ef69296b5eeab0693511cfde1836d0bda00bfad4
Florian Gerlinghoff <gerlinghoff@live.de>
2021-01-15 21:11:08 +0200

    Implemented Observable for real user directions input

--- Snake/RealWorld/UserDirections.cs
+++ Snake/RealWorld/UserDirections.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using System.Reactive.Linq;
+using static System.ConsoleKey;
 
 namespace Snake.RealWorld
 {
     public static class UserDirections
     {
-        public static IObservable<Direction> Create()
-        {
-            return Observable.Empty<Direction>();
-        }
+        // https://stackoverflow.com/a/10678328
+        public static IObservable<Direction> Create() =>
+            Observable
+                .Defer(CreateReadKeyObservable)
+                .Repeat()
+                .Select(ConsoleKeyToDirection)
+                .Where(d => d.HasValue)
+                .Select(d => d!.Value);
+
+        private static IObservable<ConsoleKeyInfo> CreateReadKeyObservable() =>
+            Observable.Start(() => Console.ReadKey(true));
+
+        private static Direction? ConsoleKeyToDirection(ConsoleKeyInfo keyInfo) =>
+            keyInfo.Key switch
+            {
+                LeftArrow => Direction.Left,
+                RightArrow => Direction.Right,
+                UpArrow => Direction.Up,
+                DownArrow => Direction.Down,
+                _ => null
+            };
     }
 }
